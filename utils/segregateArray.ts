1ba2729f1a8e2f3c68111509bde33190a9a790c7
Henrique <henrique@falconer.com.br>
2022-03-04 02:12:30 -0300

    :construction: refactor: segregateArray one-liner

--- utils/segregateArray.ts
+++ utils/segregateArray.ts
@@ -1,12 +1,7 @@
 const segregateArray = <T>(arr: T[], segregator: (t: T) => boolean) =>
-  arr.reduce(
-    (acc, t) => {
-      const [first, second] = acc;
-      const arr = segregator(t) ? first : second;
-      arr.push(t);
-      return acc;
-    },
-    [[], []] as [T[], T[]]
+  arr.reduce<[T[], T[]]>(
+    (acc, t) => (acc[segregator(t) ? 0 : 1].push(t), acc),
+    [[], []]
   );
 
 export default segregateArray;
